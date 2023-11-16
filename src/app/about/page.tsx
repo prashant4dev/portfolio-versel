5d4873268ac8b4c83405432199b5bf56450ffc73
prashantrm <prashant@dhiway.com>
2023-11-16 18:49:23 +0530

    updated
    Signed-off-by: prashantrm <prashant@dhiway.com>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>dasdasd</h1>
+            <h1>Prashant</h1>
     </main>
   )
 }
